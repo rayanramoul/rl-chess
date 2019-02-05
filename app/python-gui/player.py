adc79b772a1e6e6841b042854df56ba8e6d72c05
raysr <raysamram@gmail.com>
2019-02-05 01:19:30 +0100

    adding image for README

--- app/python-gui/player.py
+++
@@ -1,8 +0,0 @@
-from random import *
-class Player():
-    def __init__(self, side):
-        self.side=side
-    
-    def choose(self, moves):
-        size=len(moves)
-        return randint(0,size-1)
