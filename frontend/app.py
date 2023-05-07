c2e5cfb6b656677b56e552479cd7801121bbb071
rayanramoul <raysamram@gmail.com>
2023-05-07 23:13:07 +0200

    :sparkles: create grid and load pawns

--- frontend/app.py
+++ frontend/app.py
@@ -1,58 +1,149 @@
+from dataclasses import dataclass
+
 import flet as ft
- 
+from pprint import pprint
 # Use of GestureDetector for with on_pan_update event for dragging card
 # Absolute positioning of controls within stack
 
 
-def generate_grid(page):
-    for l1 in range(8):
-        controls = []
-        for l2 in range(8):
-            controls.append(ft.Container(bgcolor=ft.colors.GREEN, width=70, height=100))
-        stack = ft.Stack(controls = controls, width=1000, height=500)
-        page.add(stack)
-        
+page = None
+@dataclass
+class Square():
+    top: int
+    left: int
+
+
+def drag(e: ft.DragUpdateEvent):
+    print("DRAGGING")
+    e.control.top = max(0, e.control.top + e.delta_y)
+    e.control.left = max(0, e.control.left + e.delta_x)
+    e.control.update()
+
     
-def main(page: ft.Page):
- 
-    def drag(e: ft.DragUpdateEvent):
-       e.control.top = max(0, e.control.top + e.delta_y)
-       e.control.left = max(0, e.control.left + e.delta_x)
-       e.control.update()
+def place(card, slot):
+    global page
+    print("PLACING ? ", (card, slot))
+    """place card to the slot"""
+    card.top = slot.top
+    card.left = slot.left
+    page.update()
+
+def generate_grid(page):
+    slots = []
+    '''
+    the_grid = ft.GridView(
+        runs_count=5,
+        max_extent=65,
+        child_aspect_ratio=1.0,
+        spacing=5,
+        run_spacing=5,
+        
+    )
+    '''
+    the_grid = ft.Stack(width=1920, height=1080)
+    slots = []
+    for l1 in range(8):
+        for l2 in range(8):
+            print("Adding :)")
+            square = ft.Container(bgcolor=ft.colors.BLUE, width=70, height=70, top=l1*70, left=l2*70, border=ft.border.all(1))
+            slots.append(square)
+            the_grid.controls.append(square)
+            if l1%2==0 and l2%2==0 or l1%2==1 and l2%2==1:
+                the_grid.controls[-1].bgcolor=ft.colors.WHITE
+            else:
+                the_grid.controls[-1].bgcolor=ft.colors.BROWN
+    
+    pawns = []
     
     def drop(e: ft.DragEndEvent):
-        if (
-            abs(e.control.top - slot.top) < 20
+        print("DROP EVENT")
+        for slot in slots:
+            print(f"slot : {slot.top}, {slot.left}")
+            print(f"card : {e.control.top}, {e.control.left}")
+            if (
+                abs(e.control.top - slot.top) < 20
             and abs(e.control.left - slot.left) < 20
-        ):
-            place(e.control, slot)
-        e.control.update()
+            ):
+                place(e.control, slot)
+                e.control.update()
+                return None
+        #bounce_back(solitaire, e.control)
+        # e.control.update()
 
-    def place(card, slot):
-        """place card to the slot"""
-        card.top = slot.top
-        card.left = slot.left
-        page.update()
- 
-    card = ft.GestureDetector(
-       mouse_cursor=ft.MouseCursor.MOVE,
-       drag_interval=5,
-       on_pan_update=drag,
-       on_pan_end=drop,
-       left=0,
-       top=0,
-       content=ft.Image(
-        src=f"./frontend/assets/bB.png",
-        width=100,
-        height=100,
-        fit=ft.ImageFit.CONTAIN,
-    ),
-    )   
-    slot = ft.Container(
-    width=70, height=100, left=200, top=0, border=ft.border.all(1)
-    )
-    page.add(ft.Stack(controls = [slot, card], width=1000, height=500))
-    generate_grid(page)
- 
+    """"""
+    black_parts = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+    white_parts = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+    for i in range(8):
+        card = ft.GestureDetector(
+        mouse_cursor=ft.MouseCursor.MOVE,
+        drag_interval=5,
+        on_pan_update=drag,
+        on_pan_end=drop,
+        left=i*70,
+        top=70,
+        content=ft.Image(
+            src=f"./assets/bP.png",
+            width=70,
+            height=70,
+            fit=ft.ImageFit.CONTAIN,
+        ))
+    
+        pawns.append(card)
+        
+        card = ft.GestureDetector(
+        mouse_cursor=ft.MouseCursor.MOVE,
+        drag_interval=5,
+        on_pan_update=drag,
+        on_pan_end=drop,
+        left=i*70,
+        top=0,
+        content=ft.Image(
+            src=f"./assets/b{black_parts[i]}.png",
+            width=70,
+            height=70,
+            fit=ft.ImageFit.CONTAIN,
+        ))
+        pawns.append(card)
+        
+        
+        card = ft.GestureDetector(
+        mouse_cursor=ft.MouseCursor.MOVE,
+        drag_interval=5,
+        on_pan_update=drag,
+        on_pan_end=drop,
+        left=i*70,
+        top=420,
+        content=ft.Image(
+            src=f"./assets/wP.png",
+            width=70,
+            height=70,
+            fit=ft.ImageFit.CONTAIN,
+        ))
+        pawns.append(card)
+        
+        card = ft.GestureDetector(
+        mouse_cursor=ft.MouseCursor.MOVE,
+        drag_interval=5,
+        on_pan_update=drag,
+        on_pan_end=drop,
+        left=i*70,
+        top=500,
+        content=ft.Image(
+            src=f"./assets/w{white_parts[i]}.png",
+            width=70,
+            height=70,
+            fit=ft.ImageFit.CONTAIN,
+        ))
+        pawns.append(card)
+        
+    pawns = ft.Stack(controls=pawns, width=500, height=500)    
+    return the_grid, pawns
+    
+    
+def main(the_page: ft.Page): 
+    global page 
+    page = the_page     
+    the_grid, pawns = generate_grid(the_page)
+    the_page.add(ft.Stack(controls=[the_grid, pawns], width=1920, height=1080))
 
 ft.app(target=main)
