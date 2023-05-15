7d6f32a33704d5959eb55c0b12944af5e580c05d
rayanramoul <raysamram@gmail.com>
2023-05-15 20:47:11 +0200

    :sparkles: adding new layouts

--- frontend/app.py
+++ frontend/app.py
@@ -25,7 +25,47 @@ LEFT_POSITION_BOARD = 60
 
 move_sound = ft.Audio(src=os.path.join(ACTUAL_PATH, "sounds/move.wav"))
 wrong_move_sound = ft.Audio(src=os.path.join(ACTUAL_PATH, "sounds/wrong_move.mp3"))
-background_music = ft.Audio(src=os.path.join(ACTUAL_PATH, "sounds/background_music.mp3"), autoplay=True)
+#background_music = ft.Audio(src=os.path.join(ACTUAL_PATH, "sounds/background_music.mp3"), autoplay=True)
+
+
+player_box1 = None
+player_box2 = None
+
+def update_turn_visual():
+    if white_turn:
+        player_box1.content.controls[-1].color = "green"
+        player_box2.content.controls[-1].color = "red"
+        
+    else:
+        player_box2.content.controls[-1].color = "green"
+        player_box1.content.controls[-1].color = "red"
+    player_box1.content.controls[-1].update()
+    player_box2.content.controls[-1].update()
+    
+def player_box(player_name, player_color):
+    player_icon = ft.Image(
+            src=os.path.join(ACTUAL_PATH, f"assets/wB.png"),
+            width=70,
+            height=70,
+            fit=ft.ImageFit.CONTAIN,
+            color=player_color,
+             
+    )
+    player_column = ft.Column(
+        controls=[
+            ft.Text(value=player_name, color=player_color),
+            player_icon,
+        ],
+        width=600,
+        height=200,
+    )
+    player_container = ft.Container(
+        content=player_column,
+        bgcolor=ft.colors.WHITE,
+    )
+    return player_container 
+
+
 
 def drag(e: ft.DragUpdateEvent):
     e.control.top = max(0, e.control.top + e.delta_y)
@@ -112,6 +152,7 @@ def generate_grid(page):
             move_sound.play()
             board.push(move)
             white_turn = not white_turn
+            update_turn_visual()
             turn_text.value = f"Turn: {'White' if white_turn else 'Black'}"
             if board.is_game_over():
                 turn_text.value += "\n - Game over!"
@@ -227,9 +268,35 @@ def generate_grid(page):
 def main(the_page: ft.Page): 
     global turn_text
     global page 
+    global player_box1, player_box2
     
-    turn_text = ft.Text(value="TURN : WHITE", color="white", left=250, size=50)
-    right_stack = ft.Stack(width=500, height=1080, controls=[turn_text])
+    turn_text = ft.Text(value="TURN : WHITE", color="green") #, left=250, size=50)
+    player_box1 = player_box("Player 1", "blue")
+    player_box2 = player_box("Player 2", "red")
+    
+    previous_moves_list = ft.ListView(expand=True, spacing=10, item_extent=50, width=500, height=100)
+    for i in range(5000):
+        previous_moves_list.controls.append(ft.Text(f"Move {i}"))
+    middle_right_row = ft.Row(width=500, height=200, controls=[previous_moves_list])
+    
+    controls_row = ft.Row(
+            [
+                ft.IconButton(
+                    icon=ft.icons.ARROW_BACK_IOS_NEW_OUTLINED,
+                    icon_color="blue400",
+                    icon_size=40,
+                    tooltip="Pause record",
+                ),
+                ft.IconButton(
+                    icon=ft.icons.ARROW_FORWARD_IOS_OUTLINED,
+                    icon_color="pink600",
+                    icon_size=40,
+                    tooltip="Delete record",
+                ),
+            ]
+        )
+    
+    right_stack = ft.Column(width=500, height=1080, controls=[turn_text, player_box1, middle_right_row, controls_row, player_box2])
     
     
     # death_area = ft.Stack(width=200, height=200, ft.Container(bgcolor=ft.colors.RED, top=400, left=600)
@@ -237,9 +304,8 @@ def main(the_page: ft.Page):
     
     
     
-    
     the_grid, pawns = generate_grid(the_page)
-    game_stack = ft.Stack(controls=[the_grid, pawns, move_sound, wrong_move_sound, background_music],  width=1920, height=1100)
+    game_stack = ft.Stack(controls=[the_grid, pawns, move_sound, wrong_move_sound],  width=800, height=1100)
     main_row = ft.Row(controls=[game_stack, right_stack], width=1920, height=1080)
     
     the_page.add(main_row)
